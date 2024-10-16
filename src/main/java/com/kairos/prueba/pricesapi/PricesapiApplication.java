ae7b318da15700cfc1d80589777936a43c186632
Francisco Cilleruelo <francisco.cilleruelot@externos.santalucia.es>
2024-10-16 13:39:12 +0200

    feat: First basic commit
    Initial commit

---
+++ src/main/java/com/kairos/prueba/pricesapi/PricesapiApplication.java
@@ -0,0 +1,13 @@
+package com.kairos.prueba.pricesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricesapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kairos/prueba/pricesapi/PricesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kairos.prueba.pricesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
